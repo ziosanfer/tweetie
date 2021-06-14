466c8b5a5b1a2a856b87792e36ad1821fd306183
ziosanfer <ziosanfer@gmail.com>
2021-06-14 10:42:05 -0700

    pushing tweetie

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>A Twitter Clone Named Tweetie</p>
+        <footer>
+          Copyright &copy; 2021 Tweetie
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
